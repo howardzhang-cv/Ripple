1fa3862359aa77312d8eea3b29bdd84c12fad878
Andrew Kim <adkim2999@gmail.com>
2018-11-03 18:43:12 -0700

    added channels fragment and adapter

---
+++ app/src/main/java/co/ripple/ripple/ChannelsAdapter.java
@@ -0,0 +1,7 @@
+package co.ripple.ripple;
+
+/**
+ * Created by Andrew Kim on 11/3/18.
+ */
+public class ChannelsAdapter {
+}
---
+++ app/src/main/java/co/ripple/ripple/ChannelsFragment.java
@@ -0,0 +1,9 @@
+package co.ripple.ripple;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Andrew Kim on 11/3/18.
+ */
+public class ChannelsFragment extends Fragment {
+}
